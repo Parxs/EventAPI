40919a61be0ff738027d9fac6f04ae260c789424
parxs <christopher.naschberger@student.uibk.ac.at>
2018-05-16 21:26:39 +0200

    adds support for swagger

--- src/main/java/at/ac/uibk/EventApiApplication.java
+++ src/main/java/at/ac/uibk/EventApiApplication.java
@@ -2,12 +2,16 @@ package at.ac.uibk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "at.ac.uibk" })
 public class EventApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventApiApplication.class, args);
 	}
 }
-	
